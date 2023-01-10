c39e55ccc0e9a238dd404d03db1852a160fe86f6
nnsahu02 <nnsahu2022@gmail.com>
2023-01-10 12:44:46 +0530

    Done

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const userController = require('../controller/userController')
 const adminController = require('../controller/adminController')
+const auth = require('../middleware/auth')
 
 //USER APIs
 //REGISTER USER
@@ -11,30 +12,30 @@ router.post('/Register', userController.registerUser)
 router.post('/login', userController.login)
 
 //GET SLOTES PER DAY
-router.get('/slots', userController.getSlot)
+router.get('/:userId/slots', auth.authentication, auth.authorization, userController.getSlot)
 
 //REGISTER SLOT
-router.post('/:userId/slot/register', userController.registerSlot)
+router.post('/:userId/slot/register', auth.authentication, auth.authorization, userController.registerSlot)
 
 //Update registered vaccine slot 
-router.put('/:userId/update/slot', userController.updateRegisteredSlot)
+router.put('/:userId/update/slot', auth.authentication, auth.authorization, userController.updateRegisteredSlot)
 
 
 
 //ADMIN APIs
 //createSlot
-router.post('/:adminId/slots/day', adminController.createSlot)
+router.post('/:adminId/slots/day', auth.authentication, auth.adminAuthorization, adminController.createSlot)
 
 //GET SLOTS PER DAY
-router.get("/:adminId/slots/day", adminController.getSlot)
+router.get("/:adminId/slots/day", auth.authentication, auth.adminAuthorization, adminController.getSlot)
 
 //GET USER REGISTERED FOR SLOT DATA  
-router.get('/:adminId/user/registerslot', adminController.getRegisteredSlot)
+router.get('/:adminId/user/registerslot', auth.authentication, auth.adminAuthorization, adminController.getRegisteredSlot)
 
 //UPDATE VACCINATION STATUS
-router.put("/:adminId/update/status", adminController.updateVaccineStatus)
+router.put("/:adminId/update/status", auth.authentication, auth.adminAuthorization, adminController.updateVaccineStatus)
 
 //Admin vaccine slots API
-router.get("/:adminId/register/vaccine/slot", adminController.registerVaccineSlot)
+router.get("/:adminId/register/vaccine/slot", auth.authentication, auth.adminAuthorization, adminController.registerVaccineSlot)
 
 module.exports = router
